60fc36a2884feb5978588e709b8c5a4bf1c424e6
Logan Woodbury <testare.i9z@gmail.com>
2018-03-22 10:35:40 -0600

    Create simple client server
    Created simple client server functionality.

--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,10 @@ import Lib
 
 main :: IO ()
 main = someFunc
+
+someFunc :: IO ()
+someFunc = do
+    putStrLn "Client or Server (s/c)?"
+    h <- getLine
+    putStrLn $ "You said: (" ++ h ++ ")\n"
+    if (h == "c") then runClient else runServer
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Network.Socket as NS
+import Network.Socket.ByteString (recv,sendAll)
+import qualified Control.Exception as E
+import qualified Data.ByteString.Char8 as C
+
+--import qualified Network.Simple.TCP as TCP
+
+runClient :: IO ()
+runClient = NS.withSocketsDo $ do
+        addr <- resolve "127.0.0.1" "12482"
+        E.bracket (open addr) NS.close talk
+    where
+        resolve host port = do
+            let hints = NS.defaultHints { NS.addrSocketType = NS.Stream }
+            addr:_ <- NS.getAddrInfo (Just hints) (Just host) (Just port)
+            return addr
+        open addr = do
+            sock <- NS.socket (NS.addrFamily addr) (NS.addrSocketType addr) (NS.addrProtocol addr)
+            NS.connect sock $ NS.addrAddress addr
+            return sock
+        talk sock = do
+            sendAll sock "Hello, world!"
+            msg <- recv sock 1024
+            putStr "Received: "
+            C.putStrLn msg
+
+runServer :: IO ()
+runServer = NS.withSocketsDo $ do
+    address:_ <- NS.getAddrInfo (
+        Just (NS.defaultHints 
+                { NS.addrFlags = [NS.AI_PASSIVE]
+                , NS.addrSocketType = NS.Stream 
+                }))
+        Nothing 
+        (Just "12482")
+    putStrLn $ show address
+    E.bracket (openServerSocket address) closeServerSocket runServerLoop
+
+
+openServerSocket :: NS.AddrInfo -> IO NS.Socket
+openServerSocket address = do
+    sock <- NS.socket (NS.addrFamily address) (NS.addrSocketType address) (NS.addrProtocol address)
+    NS.setSocketOption sock NS.ReuseAddr 1
+    NS.bind sock (NS.addrAddress address)
+    NS.listen sock 5
+    return sock
+
+closeServerSocket :: NS.Socket -> IO ()
+closeServerSocket = NS.close
+
+runServerLoop :: NS.Socket -> IO ()
+runServerLoop sock = do
+    (conn, peer) <- NS.accept sock
+    putStrLn $ "We got a connection! From: " ++ (show peer)
+    msg <- recv conn 1024
+    putStr "Received: "
+    C.putStrLn msg
+    sendAll conn "Goodbye scrub!"
+    NS.close conn
